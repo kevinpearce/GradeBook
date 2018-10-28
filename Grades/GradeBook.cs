72e4a8d4099adc1d5ea1b5a06e85b5b69eb3c093
Kevin Pearce <kevinjohnpearce@googlemail.com>
2018-10-28 11:18:44 +0000

    Fields, Properties & Events

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -10,6 +10,7 @@ namespace Grades
     {
         public GradeBook()
         {
+            _name = "Empty";
             grades = new List<float>();
         }
 
@@ -33,8 +34,34 @@ namespace Grades
             grades.Add(grade);
         }
 
-        public string Name;
+        //property (also has a backing "Field")
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    if(_name != value)
+                    {
+                        NameChangedEventArgs args = new NameChangedEventArgs();
+                        args.ExistingName = _name;
+                        args.NewName = value;
+                        
+                        NameChanged(this, args);
+                    }
 
+                    _name = value;
+                }
+            }
+        }
+
+        public event NameChangedDelegate NameChanged;
+
+        private string _name;
         private List<float> grades;
     }
 }
